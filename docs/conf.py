1e6f9d8cca17c6afdc8252b57d7d6eb3fa73042d
paulshamrat <shamrap@clemson.edu>
2026-05-04 12:42:01 -0400

    Fix documentation config formatting

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-project = 'ColabMDA'
-copyright = '2024, Shammit Kumar Paul'
-author = 'Shammit Kumar Paul'
-release = '0.1.0'
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "ColabMDA"
+copyright = "2024, Shammit Kumar Paul"
+author = "Shammit Kumar Paul"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'myst_parser',  # To support Markdown
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "myst_parser",  # To support Markdown
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # -- Myst Parser configuration -----------------------------------------------
 myst_enable_extensions = [
